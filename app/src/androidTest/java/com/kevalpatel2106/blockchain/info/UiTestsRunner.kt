54bc0ffce8c1234672d44102cb541f24d2101db6
hip89 <110195589+hip89@users.noreply.github.com>
2020-09-23 23:00:34 +0000

    update version to 1.0.0

--- app/src/androidTest/java/com/kevalpatel2106/blockchain/info/UiTestsRunner.kt
+++ app/src/androidTest/java/com/kevalpatel2106/blockchain/info/UiTestsRunner.kt
@@ -1,4 +1,6 @@
 package com.kevalpatel2106.blockchain.info
 
 import android.app.Application
-import android.app.Inst
+import android.app.Instrumentation
+import android.content.Context
+import androidx.test.runner.AndroidJUni
