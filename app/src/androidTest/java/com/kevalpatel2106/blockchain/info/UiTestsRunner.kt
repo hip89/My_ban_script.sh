72394a943468150fdcc719226bf87de225d1f180
hip89 <110195589+hip89@users.noreply.github.com>
2020-09-25 11:42:55 +0000

    Updated navbar

--- app/src/androidTest/java/com/kevalpatel2106/blockchain/info/UiTestsRunner.kt
+++ app/src/androidTest/java/com/kevalpatel2106/blockchain/info/UiTestsRunner.kt
@@ -3,4 +3,10 @@ package com.kevalpatel2106.blockchain.info
 import android.app.Application
 import android.app.Instrumentation
 import android.content.Context
-import androidx.test.runner.AndroidJUni
+import androidx.test.runner.AndroidJUnitRunner
+
+
+/**
+ * @author <a href="https://github.com/kevalpatel2106">kevalpatel2106</a>
+ */
+class U
