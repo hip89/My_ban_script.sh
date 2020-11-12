8bb09fc68c9b47705a22b66c768eb71402ac6e9d
hip89 <110195589+hip89@users.noreply.github.com>
2020-11-12 16:59:54 +0000

    update architecture

--- app/src/main/java/com/kevalpatel2106/blockchain/info/bin/Transaction.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/bin/Transaction.kt
@@ -3,3 +3,7 @@ package com.kevalpatel2106.blockchain.info.bin
 import com.kevalpatel2106.blockchain.info.utils.convertToBtc
 
 data class Transaction(
+        val fee: Long,
+        val result: Long,
+        val balance: Long,
+        val time: 
