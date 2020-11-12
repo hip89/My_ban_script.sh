469638f3a566f24cdaeea70a7c14adad82e76f3d
hip89 <110195589+hip89@users.noreply.github.com>
2020-11-12 10:44:09 +0000

    r8169: use unlimited DMA burst for TX

---
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/BIApplication.kt
@@ -0,0 +1,7 @@
+
+package com.kevalpatel2106.blockchain.info
+
+import com.facebook.stetho.Stetho
+import com.kevalpatel2106.blockchain.info.di.AppDiComponent
+import com.kevalpatel2106.blockchain.info.di.AppDiModule
+import com.kevalpatel2106.blockchain.info.di.DaggerAppDiComponent
