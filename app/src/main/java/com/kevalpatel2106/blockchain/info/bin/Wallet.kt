847971b43b9ed404c85eb67e87881229beff1a50
hip89 <110195589+hip89@users.noreply.github.com>
2020-11-14 18:37:01 +0000

    1.1.25

--- app/src/main/java/com/kevalpatel2106/blockchain/info/bin/Wallet.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/bin/Wallet.kt
@@ -3,4 +3,6 @@ package com.kevalpatel2106.blockchain.info.bin
 import com.kevalpatel2106.blockchain.info.utils.convertToBtc
 
 
-data class
+data class Wallet(
+        val finalBalance: Long,
+        val numberOfTransactions: Lo
