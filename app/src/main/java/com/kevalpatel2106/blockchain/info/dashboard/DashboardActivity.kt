c19cc61d16e961d2f05eab8e2c985f6701574527
hip89 <110195589+hip89@users.noreply.github.com>
2020-11-30 13:29:48 +0000

    seeds

--- app/src/main/java/com/kevalpatel2106/blockchain/info/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/dashboard/DashboardActivity.kt
@@ -55,3 +55,5 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     private fun setWalletInfo() {
+        model.wallet.nullSafeObserve(this) {
+            wallet_balance_tv.text = it.for
