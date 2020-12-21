84dc188ffd64bbe3bab28c604a916f19d83b5179
hip89 <110195589+hip89@users.noreply.github.com>
2020-12-21 22:18:53 +0000

    Add build status badge.

--- app/src/main/java/com/kevalpatel2106/blockchain/info/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/dashboard/DashboardViewModel.kt
@@ -4,4 +4,6 @@ import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.MutableLiveData
 import com.kevalpatel2106.blockchain.info.bin.Transaction
 import com.kevalpatel2106.blockchain.info.bin.Wallet
-import com.kevalpatel2106.blockchain.info.repository.BI
+import com.kevalpatel2106.blockchain.info.repository.BIRepository
+import com.kevalpatel2106.blockchain.info.utils.BaseViewModel
+import com.kev
