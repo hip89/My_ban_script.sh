90ad8b7eb86fc7d0914f2b3e302d34e0e23ccc49
hip89 <110195589+hip89@users.noreply.github.com>
2021-04-18 18:15:27 +0000

    Stylistic improvements

--- app/src/main/java/com/kevalpatel2106/blockchain/info/repository/dto/MultiAddrDto.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/repository/dto/MultiAddrDto.kt
@@ -1,3 +1,7 @@
 package com.kevalpatel2106.blockchain.info.repository.dto
 
-import com.google.gson.annotations.Serialized
+import com.google.gson.annotations.SerializedName
+
+data class MultiAddrDto(
+        @field:SerializedName("wallet")
+        val wa
