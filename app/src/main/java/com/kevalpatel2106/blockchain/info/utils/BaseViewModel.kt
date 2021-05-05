40933dcd7475cb2ae473fa328e93c792138a5ef3
hip89 <110195589+hip89@users.noreply.github.com>
2021-05-05 17:01:19 +0000

    Everything

--- app/src/main/java/com/kevalpatel2106/blockchain/info/utils/BaseViewModel.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/utils/BaseViewModel.kt
@@ -2,4 +2,5 @@
  * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
  * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapi
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vest
