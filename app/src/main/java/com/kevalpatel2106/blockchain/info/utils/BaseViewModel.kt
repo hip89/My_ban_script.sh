3a562fbc1b308884df109c3f8aebc546793ce75c
hip89 <110195589+hip89@users.noreply.github.com>
2021-05-05 16:44:15 +0000

    proofing

--- app/src/main/java/com/kevalpatel2106/blockchain/info/utils/BaseViewModel.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/utils/BaseViewModel.kt
@@ -1,4 +1,5 @@
 /*
  * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam s
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapi
