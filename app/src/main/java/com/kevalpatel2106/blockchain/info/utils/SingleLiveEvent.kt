18fb8090619600ad46456fb53c4eca36c82f8463
hip89 <110195589+hip89@users.noreply.github.com>
2021-05-11 13:25:42 +0000

    Update ignore list.

--- app/src/main/java/com/kevalpatel2106/blockchain/info/utils/SingleLiveEvent.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/utils/SingleLiveEvent.kt
@@ -1,4 +1,5 @@
 /*
  * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam se
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sa
