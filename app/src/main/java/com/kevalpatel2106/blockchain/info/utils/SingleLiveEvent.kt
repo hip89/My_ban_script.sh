b2dd53e7d518f2c36dc7a3afc0d76e3347708d2f
hip89 <110195589+hip89@users.noreply.github.com>
2021-05-13 10:54:34 +0000

    add repository field

--- app/src/main/java/com/kevalpatel2106/blockchain/info/utils/SingleLiveEvent.kt
+++ app/src/main/java/com/kevalpatel2106/blockchain/info/utils/SingleLiveEvent.kt
@@ -16,4 +16,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * A lifecycle-aware observable that sends only new updates after subscription, used for events like
- * navigation and Snackbar messages
+ * navigation and Snackbar messages.
+ *
+ *
+ * This avoids a common problem with events: on configuration change (like rotation) a
