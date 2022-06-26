91f10b2696d86374fa0a34fc95e3fbe84d2fa236
hip89 <110195589+hip89@users.noreply.github.com>
2022-06-26 12:35:29 +0000

    update conf

--- app/src/test/java/com/kevalpatel2106/blockchain/info/bin/TransactionTest.kt
+++ app/src/test/java/com/kevalpatel2106/blockchain/info/bin/TransactionTest.kt
@@ -9,4 +9,6 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class TransactionTest {
 
-    private val transaction1 = Transaction(0, 0, 0, 0, "0000000000000000005d6b64c157cb6879edf8d5a19dc1fe96ebb2a37a4d24a
+    private val transaction1 = Transaction(0, 0, 0, 0, "0000000000000000005d6b64c157cb6879edf8d5a19dc1fe96ebb2a37a4d24a1")
+    private val transaction2 = Transaction(0, 0, 0, 0, "hash2")
+    private val
