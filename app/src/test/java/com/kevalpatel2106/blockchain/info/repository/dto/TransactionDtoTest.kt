f7320a01b70089f3ffc17987115695060116e40a
hip89 <110195589+hip89@users.noreply.github.com>
2022-08-07 20:16:49 +0000

    remove .gitmodules

--- app/src/test/java/com/kevalpatel2106/blockchain/info/repository/dto/TransactionDtoTest.kt
+++ app/src/test/java/com/kevalpatel2106/blockchain/info/repository/dto/TransactionDtoTest.kt
@@ -2,4 +2,8 @@ package com.kevalpatel2106.blockchain.info.repository.dto
 
 import org.junit.Assert.*
 import org.junit.Test
-import org.junit.runner.R
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class TransactionDt
