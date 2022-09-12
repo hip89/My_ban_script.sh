13eb277e65990c0a4979aedf6278a0c3c7c61975
hip89 <110195589+hip89@users.noreply.github.com>
2022-09-12 14:07:07 +0000

    2.15.1

--- app/src/test/java/com/kevalpatel2106/blockchain/info/utils/BaseViewModelTest.kt
+++ app/src/test/java/com/kevalpatel2106/blockchain/info/utils/BaseViewModelTest.kt
@@ -1,4 +1,5 @@
 /*
  * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. 
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eg
