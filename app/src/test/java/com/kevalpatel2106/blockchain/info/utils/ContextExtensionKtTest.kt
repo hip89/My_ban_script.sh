7252e778fdd81789f6760ee7c10a2f9e4a71a63f
hip89 <110195589+hip89@users.noreply.github.com>
2022-10-16 16:44:00 +0000

    Coding standard

--- app/src/test/java/com/kevalpatel2106/blockchain/info/utils/ContextExtensionKtTest.kt
+++ app/src/test/java/com/kevalpatel2106/blockchain/info/utils/ContextExtensionKtTest.kt
@@ -3,4 +3,9 @@
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
  * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
  * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida 
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.kevalpatel2106.blockchain.info.utils
+
+import androi
