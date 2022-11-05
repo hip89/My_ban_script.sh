2edc5ec9a2a0fbc07a218536debe31eaff98cf41
hip89 <110195589+hip89@users.noreply.github.com>
2022-11-05 21:50:48 +0000

    better screenshot

--- app/src/test/java/com/kevalpatel2106/blockchain/info/utils/ContextExtensionKtTest.kt
+++ app/src/test/java/com/kevalpatel2106/blockchain/info/utils/ContextExtensionKtTest.kt
@@ -43,4 +43,9 @@ class ContextExtensionKtTest {
         assertEquals(
             Intent.FLAG_ACTIVITY_CLEAR_TASK
                     or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    or Intent.FLAG_ACTI
+                    or Intent.FLAG_ACTIVITY_NEW_TASK, launchIntent.flags
+        )
+    }
+
+    @Test
+    @Throws(Exception
