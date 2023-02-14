35b8d1a7fa45f68355ef85e010602c9ea6186f97
hip89 <110195589+hip89@users.noreply.github.com>
2023-02-14 20:14:52 +0000

    staging: wlags49_h2: fix extern inline functions

--- gradlew.bat
+++ gradlew.bat
@@ -5,4 +5,7 @@
 @rem
 @rem ##########################################################################
 
-@rem Set
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRN
