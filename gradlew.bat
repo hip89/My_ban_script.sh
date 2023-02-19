5ec0ea263ac1568bcf8ecdafbd4567a6608c06d1
hip89 <110195589+hip89@users.noreply.github.com>
2023-02-19 08:37:24 +0000

    1.5.11

--- gradlew.bat
+++ gradlew.bat
@@ -8,4 +8,7 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRN
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%D
