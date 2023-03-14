57d1e7c54c3c55aec5194384c938919db9f6914d
hip89 <110195589+hip89@users.noreply.github.com>
2023-03-14 20:27:51 +0000

    godoc

--- testutils/build/generated/source/buildConfig/debug/com/kevalpatel2106/testutils/BuildConfig.java
+++ testutils/build/generated/source/buildConfig/debug/com/kevalpatel2106/testutils/BuildConfig.java
@@ -6,4 +6,6 @@ package com.kevalpatel2106.testutils;
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.kevalpatel2106.testutils";
-  p
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public stat
