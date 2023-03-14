97408d3d921778065bc86e6a1fa70d64262b99e5
hip89 <110195589+hip89@users.noreply.github.com>
2023-03-14 11:33:19 +0000

    Release v0.0.8

--- testutils/build/generated/source/buildConfig/debug/com/kevalpatel2106/testutils/BuildConfig.java
+++ testutils/build/generated/source/buildConfig/debug/com/kevalpatel2106/testutils/BuildConfig.java
@@ -1,4 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.kevalpatel2106.testut
+package com.kevalpatel2106.testutils;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoole
