73d6b59ca8598cd85946891ff4ba638e1fe9daed
hip89 <110195589+hip89@users.noreply.github.com>
2023-03-16 22:53:49 +0000

    sdsdsd

---
+++ testutils/src/main/java/com/kevalpatel2106/testutils/FileReader.kt
@@ -0,0 +1,24 @@
+
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.kevalpatel2106.testutils
+
+import android.content.Context
+import java.io.*
+
+/**
+ * Created by Kevalpatel2106 on 02-Jan-18.
+ *
+ * @author <a href="https://github.com/kevalpatel2106">kevalpatel2106</a>
+ */
+object FileReader {
+
+    private fun getStringFromInputStream(`is`: InputStream): String {
+        var br: BufferedReader? = null
+        val sb = StringBuilder()
