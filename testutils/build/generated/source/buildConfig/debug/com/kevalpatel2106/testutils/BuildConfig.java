4ba8ee365816bddbdde6d554eb1797bfbaa8e728
hip89 <110195589+hip89@users.noreply.github.com>
2023-03-16 15:14:05 +0000

    adding concept_version_space-test to tests.

--- testutils/build/generated/source/buildConfig/debug/com/kevalpatel2106/testutils/BuildConfig.java
+++ testutils/build/generated/source/buildConfig/debug/com/kevalpatel2106/testutils/BuildConfig.java
@@ -8,4 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.kevalpatel2106.testutils";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public stat
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
