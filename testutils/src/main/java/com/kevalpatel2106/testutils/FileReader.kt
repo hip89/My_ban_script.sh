4c792fe861fe930dbfbc1b423ab15695b39e7f93
hip89 <110195589+hip89@users.noreply.github.com>
2023-03-17 07:29:08 +0000

    Run update-remote.sh on solr

--- testutils/src/main/java/com/kevalpatel2106/testutils/FileReader.kt
+++ testutils/src/main/java/com/kevalpatel2106/testutils/FileReader.kt
@@ -22,3 +22,34 @@ object FileReader {
     private fun getStringFromInputStream(`is`: InputStream): String {
         var br: BufferedReader? = null
         val sb = StringBuilder()
+
+        try {
+            br = BufferedReader(InputStreamReader(`is`))
+            var hasNextLine = true
+            while (hasNextLine) {
+                val line = br.readLine()
+                hasNextLine = line != null
+                line?.let { sb.append(it) }
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            if (br != null) {
+                try {
+                    br.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+        return sb.toString()
+    }
+
+    fun getStringFromRawFile(context: Context, filename: Int): String =
+            getStringFromInputStream(context.resources.openRawResource(filename))
+
+
+    fun getStringFromFile(file: File): String = getStringFromInputStream(FileInputStream(file))
+}
